c9c799614b163b6b55cf942071b2cbad4b6b016b
liuyang1817700453 <66665269+liuyang1817700453@users.noreply.github.com>
2020-06-09 15:29:05 +0800

    Add files via upload

---
+++ 16.zhuangtaimoshi/src/lianxi/RestState.java
@@ -0,0 +1,10 @@
+package lianxi;
+
+//下班休息状态
+public class RestState implements State
+{
+	public void writeProgram(Work work)
+	{
+		System.out.println("当前时间：" + work.getHour() + "点 下班回家了");
+	}
+}
